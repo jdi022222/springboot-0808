9b1f6b95c776ae83dc4dfdfbbc6eb85162b237f0
jdi022222 <jdi022222@gmail.com>
2022-08-10 11:29:51 +0900

    Question Entity 추가

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -213,7 +213,7 @@ public class MainController {
         private int id;
         private int age;
         private String name;
-        
+
         public Person(int age, String name) {
             this.age = age;
             this.name = name;
