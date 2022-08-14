3dc9bd6505b7f34f9822ee834ae6ddb10ea94da4
jdi022222 <97517890+jdi022222@users.noreply.github.com>
2022-08-14 22:16:26 +0900

    findBySubjectAndContent 추가

--- src/main/java/com/ll/exam/sbb/QuestionRepository.java
+++ src/main/java/com/ll/exam/sbb/QuestionRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
+
+    Question findBySubjtAndContenect(String s, String s1);
 }
