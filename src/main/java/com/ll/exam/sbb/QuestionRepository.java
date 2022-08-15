8c17033d85b1803c894c7ed53c5e7f58a80db866
jdi022222 <97517890+jdi022222@users.noreply.github.com>
2022-08-16 01:56:57 +0900

    question truncate 테스트케이스 추가

--- src/main/java/com/ll/exam/sbb/QuestionRepository.java
+++ src/main/java/com/ll/exam/sbb/QuestionRepository.java
@@ -1,7 +1,10 @@
 package com.ll.exam.sbb;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
@@ -10,4 +13,9 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubjectAndContent(String s, String s1);
 
     List<Question> findBySubjectLike(String s);
+
+    @Transactional
+    @Modifying
+    @Query(value = "truncate question", nativeQuery = true)
+    void truncate();
 }
