653c2ca78ebfa251d8f7ec503276b086a35701da
jdi022222 <97517890+jdi022222@users.noreply.github.com>
2022-08-18 02:52:45 +0900

    답변 폼 체크

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -1,14 +1,16 @@
 package com.ll.exam.sbb.question;
 
+import com.ll.exam.sbb.answer.AnswerForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import javax.validation.Valid;
 import java.util.List;
-
 @RequestMapping("/question")
 @Controller
 @RequiredArgsConstructor // 생성자 주입
@@ -23,45 +25,32 @@ public class QuestionController {
     // @Autowired // 필드 주입
     private final QuestionService questionService;
 
-    @RequestMapping("/list")
+    @GetMapping("/list")
     // 이 자리에 @ResponseBody가 없으면 resources/question_list/question_list.html 파일을 뷰로 삼는다.
     public String list(Model model) {
         List<Question> questionList = questionService.getList();
-
         // 미래에 실행된 question_list.html 에서
         // questionList 라는 이름으로 questionList 변수를 사용할 수 있다.
         model.addAttribute("questionList", questionList);
-
         return "question_list";
     }
 
-    @RequestMapping("/detail/{id}")
-    public String detail(Model model, @PathVariable int id) {
+    @GetMapping("/detail/{id}")
+    public String detail(Model model, @PathVariable int id, AnswerForm answerForm) {
         Question question = questionService.getQuestion(id);
 
         model.addAttribute("question", question);
-
         return "question_detail";
     }
-
-    @RequestMapping("/create")
-    public String create(Model model, @PathVariable int id) {
-        Question question = questionService.getQuestion(id);
-
-        return "question_detail";
-    }
-
     @GetMapping("/create")
     public String questionCreate(QuestionForm questionForm) {
         return "question_form";
     }
-
     @PostMapping("/create")
     public String questionCreate(Model model, @Valid QuestionForm questionForm, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "question_form";
         }
-
         questionService.create(questionForm.getSubject(), questionForm.getContent());
         return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
     }
