f7eb5258973ab8d9927ce23b084ecc7c9e6cc0f8
jdi022222 <97517890+jdi022222@users.noreply.github.com>
2022-08-18 10:02:46 +0900

    페이지 함수 테스트

--- src/test/java/com/ll/exam/sbb/QuestionRepositoryTests.java
+++ src/test/java/com/ll/exam/sbb/QuestionRepositoryTests.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -91,6 +94,15 @@ public class QuestionRepositoryTests {
 
         Question q = all.get(0);
         assertThat(q.getSubject()).isEqualTo("sbb가 무엇인가요?");
+    }
+
+    @Test
+    void findAllPageable() {
+        // Pageble : 한 페이지에 몇개의 아이템이 나와야 하는지 + 현재 몇 페이지인지)
+        Pageable pageable = PageRequest.of(0, lastSampleDataId);
+        Page<Question> page = questionRepository.findAll(pageable);
+
+        assertThat(page.getTotalPages()).isEqualTo(1);
     }
 
     @Test
